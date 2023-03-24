67c10bbc2e4f0a5842a10dca41173b592ff2b7dd
Nguyễn Bảo Long <nblong@mac.com>
2023-03-24 07:49:58 +0700

    gen & visualize data cifar

---
+++ src/data/cifar_loader.py
@@ -0,0 +1,27 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import json
+
+class CifarDataset(Dataset):
+    def __init__(self, file_path: str) -> None:
+        super().__init__()
+
+        fi = open(file_path, 'r')
+        data_tmp: dict = json.load(fi)
+        self.X:torch.Tensor = torch.cat([torch.Tensor(data_tmp[key]) for key in data_tmp.keys()])
+        self.y:torch.Tensor = torch.cat([torch.full((len(data_tmp[key]),), int(key)) for key in data_tmp.keys()])
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index: int):
+        # return (torch.Size([728]), torch.Size([1]))
+        return self.X[index].reshape(3,32,32), self.y[index].long()
+
+def get_loader(file_path: str, batch_size: int=32, shuffle: bool=True) -> DataLoader:
+    dataset = CifarDataset(file_path)
+    return DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle
+    )
---
+++ src/data/gen_cifar.py
@@ -0,0 +1,58 @@
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import os
+from os.path import join, isfile, isdir
+import json
+import shutil
+np.random.seed(69)
+
+from .gen_mnist import divide_data_for_clients, divide_num_sample_into_intervals, get_data_by_label, redivide_data
+
+def gen_cifar(
+        dir='./data/cifar',
+        num_training_clients=50,
+        num_testing_clients=30,
+        num_labels=10,
+        num_labels_per_client=2
+    ):
+    """generate data for non-iid scenario (no local client for testing)
+    """
+    print('\n=========== Generating data ===========')
+
+    training_intervals = int(num_training_clients/num_labels*num_labels_per_client)
+    testing_intervals = int(num_testing_clients/num_labels*num_labels_per_client)
+
+    print('\nCreate training data')
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    X_train, y_train, X_test, y_test = redivide_data('./data/cifar/', transform=transform, dataset='cifar')
+    data_train_by_label = get_data_by_label(X_train, y_train)
+    num_train_sample_in_label = divide_num_sample_into_intervals(data_train_by_label, training_intervals, num_labels)
+    all_user_train = divide_data_for_clients(data_train_by_label, num_train_sample_in_label, list(range(10)), num_training_clients, False)
+
+    print('\nCreate testing data')
+    data_test_by_label = get_data_by_label(X_test, y_test)
+    num_test_sample_in_label = divide_num_sample_into_intervals(data_test_by_label, testing_intervals, num_labels)
+    all_user_test = divide_data_for_clients(data_test_by_label, num_test_sample_in_label, list(range(10)), num_testing_clients)
+
+    print('\n=========== Write data to file ===========')
+
+    if isdir(join(dir, 'client_test')):
+        shutil.rmtree(join(dir, 'client_test'))
+    if isdir(join(dir, 'client_train')):
+        shutil.rmtree(join(dir, 'client_train'))
+    os.mkdir(join(dir, 'client_test'))
+    os.mkdir(join(dir, 'client_train'))
+
+    for user in all_user_train.keys():
+        with open(join(dir, f'client_train/{user}.json'), 'w') as fo:
+            json.dump(all_user_train[user], fo)
+
+    for user in all_user_test.keys():
+        with open(join(dir, f'client_test/{user}.json'), 'w') as fo:
+            json.dump(all_user_test[user], fo)
+
+    print('\nDone')
