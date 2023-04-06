098555c274531ceb7b5c6a89bad10383be8c0c5b
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2023-04-06 16:45:21 +0700

    format code

--- src/data/mnist_loader.py
+++ src/data/mnist_loader.py
@@ -7,17 +7,10 @@ class MnistDataset(Dataset):
     def __init__(self, file_path: str) -> None:
         super().__init__()
 
-        file_ext = file_path.split('.')[-1]
-        if file_ext == 'json':
-            fi = open(file_path, 'r')
-            data_tmp: dict = json.load(fi)
-            self.X:torch.Tensor = torch.cat([torch.Tensor(data_tmp[key]) for key in data_tmp.keys()])
-            self.y:torch.Tensor = torch.cat([torch.full((len(data_tmp[key]),), int(key)) for key in data_tmp.keys()])
-
-        elif file_ext == 'csv':
-            data_tmp = pd.read_csv(file_path).values
-            self.X = torch.Tensor(data_tmp[:,1:])
-            self.y = torch.Tensor(data_tmp[:,0])
+        fi = open(file_path, 'r')
+        data_tmp: dict = json.load(fi)
+        self.X:torch.Tensor = torch.cat([torch.Tensor(data_tmp[key]) for key in data_tmp.keys()])
+        self.y:torch.Tensor = torch.cat([torch.full((len(data_tmp[key]),), int(key)) for key in data_tmp.keys()])
 
     def __len__(self):
         return len(self.y)
